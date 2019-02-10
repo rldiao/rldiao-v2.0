93126cf032128ff896b30dffe31aceea35fb5b15
Robert D <dia.90925@gmail.com>
2019-02-10 21:39:46 +1100

    Css font bug attempt 2

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,8 +8,8 @@ import { Helmet } from "react-helmet"
 export default ({ children, data }) => (
   <div className={styles.container}>
     <Helmet>
-          {/* <meta charSet="utf-8" />
-          <title>{data.site.siteMetadata.title}</title> */}
+          <meta charSet="utf-8" />
+          {/* <title>{data.site.siteMetadata.title}</title> */}
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
     </Helmet>
     <Header />
