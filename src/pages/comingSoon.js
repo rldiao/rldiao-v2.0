cc9e485623cbf6ce713e70086b1c0b3ddf8eadbb
Robert D <dia.90925@gmail.com>
2019-02-10 21:22:31 +1100

    css bug fix

--- src/pages/comingSoon.js
+++ src/pages/comingSoon.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 export default function comingSoon() {
   return (
     <Layout>
-      <div style={{textAlign:"center", lineHeight: "400px", width: "100%", height:"400px", fontSize:"8rem"}}>Coming Soon</div>
+      <div style={{textAlign:"center", lineHeight: "400px", width: "100%", fontSize:"8rem"}}>Coming Soon</div>
     </Layout>
   )
 }
